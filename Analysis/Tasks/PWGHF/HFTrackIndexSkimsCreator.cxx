2c98fe4bb8cd78dfb87fec9368cb1002e00ea28d
Vít Kučera <vit.kucera@cern.ch>
2020-10-21 17:50:10 +0200

    Subscribe to collisionS in the creator. (good)

--- Analysis/Tasks/PWGHF/HFTrackIndexSkimsCreator.cxx
+++ Analysis/Tasks/PWGHF/HFTrackIndexSkimsCreator.cxx
@@ -47,7 +47,8 @@ struct HFTrackIndexSkimsCreator {
   Filter filterSelectTracks = (aod::hf_seltrack::isSel2Prong == true);
   using SelectedTracks = soa::Filtered<soa::Join<aod::Tracks, aod::TracksCov, aod::TracksExtra, aod::HFSelTrack>>;
 
-  void process(SelectedTracks const& tracks)
+  void process(aod::Collisions const& collisions,
+               SelectedTracks const& tracks)
   {
     // first loop over positive tracks
     for (auto trackPos1 = tracks.begin(); trackPos1 != tracks.end(); ++trackPos1) {
